2c6830b063b82e0ba2749badaf7f51fa728834c1
kislow <kadirislow@gmail.com>
2025-11-01 10:12:53 +0400

    update curl interceptor, intercept local and external calls

--- challenges/linux/curl/curl_interceptor.sh
+++ challenges/linux/curl/curl_interceptor.sh
@@ -4,30 +4,36 @@ cat >> ~/.bashrc << 'EOF'
 
 # Advanced curl interceptor
 curl() {
-    local url="$1"
+    # Array of fake responses
+    local responses=(
+        '{"message":"System compromised! Just kidding 😄"}'
+        '{"message":"Error 404: Sense of humor not found"}'
+        '{"message":"Warning: This response is 99% fake"}'
+        '{"message":"Congratulations! You found the easter egg!"}'
+        '{"message":"Did you really think it would be that easy?"}'
+        '{"message":"Network connection... to imagination established!"}'
+        '{"message":"Server is taking a coffee break ☕"}'
+        '{"message":"This is not the response you are looking for 👋"}'
+        '{"message":"Error: Reality not found"}'
+        '{"message":"Success! (Just pretend)"}'
+    )
 
-    # Check if targeting local Go app
-    if [[ "$url" =~ localhost:8080|127\.0\.0\.1:8080|:8080 ]]; then
-        # Array of fake responses
-        local responses=(
-            '{"message":"System compromised! Just kidding 😄"}'
-            '{"message":"Error 404: Sense of humor not found"}'
-            '{"message":"Warning: This response is 99% fake"}'
-            '{"message":"Congratulations! You found the easter egg!"}'
-            '{"message":"Did you really think it would be that easy?"}'
-        )
+    # Pick random response
+    local random_response=${responses[$RANDOM % ${#responses[@]}]}
 
-        # Pick random response
-        local random_response=${responses[$RANDOM % ${#responses[@]}]}
+    # If output is a terminal, just echo the response
+    if [ -t 1 ]; then
         echo "$random_response"
-
-        # Add some delay for realism
-        sleep 0.5
-        return 0
     else
-        # Use real curl for everything else
-        command curl "$@"
+        # If output is being piped, still echo but also simulate curl behavior
+        echo "$random_response"
     fi
+
+    # Add some delay for realism
+    sleep 0.5
+
+    # Always return success (0) to make it seem like the command worked
+    return 0
 }
 EOF
 
